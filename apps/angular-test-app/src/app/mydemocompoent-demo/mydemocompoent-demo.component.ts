2290d922619c8b230b568aa4aace27956600e105
Pb <rajeshkumar.pb@misys.com>
2020-07-29 15:11:50 +0530

    feat(my-component): creating new my-component

---
+++ apps/angular-test-app/src/app/mydemocompoent-demo/mydemocompoent-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ffdc-mydemocompoent-demo',
+  templateUrl: './mydemocompoent-demo.component.html',
+  styleUrls: ['./mydemocompoent-demo.component.scss']
+})
+export class MydemocompoentDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
